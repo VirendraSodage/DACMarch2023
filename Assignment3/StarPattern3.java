c3a8b467fb11c674ca3409004da535d40d9e3253
VirendraSodage <128138481+VirendraSodage@users.noreply.github.com>
2023-03-22 16:02:52 +0530

    Delete Assignment3 directory

--- Assignment3/StarPattern3.java
+++
@@ -1,9 +0,0 @@
-class StarPattern3{
-	public static void main(String args[]){
-		for(int i=0; i<=5; i++){
-			for(int j=5; j>=i; j--){
-				System.out.print("*");
-			}System.out.println(" ");
-		}
-	}
-}
